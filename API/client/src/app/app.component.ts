0ccdaa243a539a36d379785b96c20ea45a132836
rokibhassan <rokib572@gmail.com>
2023-03-11 11:44:10 +0600

    Added Navbar
    Added Navbar

--- API/client/src/app/app.component.ts
+++ API/client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'EasyStore';
+  products: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5002/api/products?pageSize=50').subscribe({
+      next: (response: any) => this.products = response.data, //What to do next
+      error: error => console.log(error), //What to do if there is an error
+      complete: () => {
+        console.log('Request completed');
+        console.log('Extra statement');
+      },
+    })
+  }
 }
